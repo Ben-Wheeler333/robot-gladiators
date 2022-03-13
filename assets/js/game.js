8d4921fc5c9e0c9a02a3c82bf44cba47f5921426
Ben-Wheeler <chemlove333@gmail.com>
2022-03-13 00:32:03 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,13 +26,15 @@ function fight() {
   if (confirmSkip) {
     window.alert(playerName + " has decided to skip this fight. Goodbye! ");
     // subtract money from playerMoney for skipping.
-    playerMoney = playerMoney - 10;
+    playerMoney = Math.max(0, playerMoney - 10);
     console.log("playerMoney", playerMoney);
     }  
 }
 
 // remove enemy's health by subtracting the amount set in the playerAttack variable
-enemyHealth = enemyHealth - playerAttack;
+var damage = randomNumber(playerAttack - 3, playerAttack);
+
+enemyHealth = Math.max(0, enemyHealth - damage);
 console.log(
   playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
 );
@@ -50,7 +52,9 @@ console.log(
     }
 
     // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack)
+    
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
@@ -84,7 +88,7 @@ var startGame = function() {
       window.alert("Welcome to Robot Gladiators!" + ( i + 1 ));
     
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
     fight(pickedEnemyName);
 
     // if we're not at the last enemy in the array
@@ -181,6 +185,13 @@ var shop = function() {
         break;
   }
 };
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+}
 
 // start the game when the page loads
 startGame();
